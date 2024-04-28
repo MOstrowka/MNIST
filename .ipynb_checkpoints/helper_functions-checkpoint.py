f1d58fa5dd6746c25a02aeab328c1485dd1bfe1d
Michał Ostrówka <m.ostrowka17@gmail.com>
2024-04-28 13:04:31 +0200

    Final

--- .ipynb_checkpoints/helper_functions-checkpoint.py
+++ .ipynb_checkpoints/helper_functions-checkpoint.py
@@ -6,34 +6,34 @@ import matplotlib.pyplot as plt
 
 def process_image(image_path, img_size=32):
     """
-    Load and preprocess an image.
+    Funkcja do wczytywania i przetwarzania obrazu.
     
     Args:
-    image_path (str): Path to the image.
-    img_size (int): Size to which the image is to be resized.
+    image_path (str): Ścieżka do obrazu.
+    img_size (int): Rozmiar, do którego obraz ma być zmieniony.
     
     Returns:
-    tf.Tensor: Processed image.
+    tf.Tensor: Przetworzony obraz.
     """
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.resize(image, [img_size, img_size])
-    image = image / 255.0  # Normalize pixel values
+    image = image / 255.0  # Normalizacja wartości pikseli
     return image
 
 def prepare_dataset_simple(dataframe, path_column, label_column, img_size=32, batch_size=32):
     """
-    Prepare a dataset from a DataFrame.
+    Funkcja do przygotowania datasetu z ramki danych.
     
     Args:
-    dataframe (pd.DataFrame): DataFrame containing image paths and labels.
-    path_column (str): Name of the column in DataFrame that contains image paths.
-    label_column (str): Name of the column in DataFrame that contains labels.
-    img_size (int): Size to which the images are to be resized.
-    batch_size (int): Batch size used during dataset creation.
+    dataframe (pd.DataFrame): DataFrame zawierający ścieżki do obrazów i etykiety.
+    path_column (str): Nazwa kolumny w DataFrame zawierającej ścieżki do obrazów.
+    label_column (str): Nazwa kolumny w DataFrame zawierającej etykiety.
+    img_size (int): Rozmiar, do którego mają być zmieniane obrazy.
+    batch_size (int): Rozmiar batcha używany podczas tworzenia datasetu.
     
     Returns:
-    tf.data.Dataset: Prepared dataset for model training.
+    tf.data.Dataset: Przygotowany dataset do trenowania modelu.
     """
     image_paths = dataframe[path_column].values
     labels = dataframe[label_column].values
@@ -44,15 +44,17 @@ def prepare_dataset_simple(dataframe, path_column, label_column, img_size=32, ba
     dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
     return dataset
 
+
 def display_random_images_with_predictions(test_dataset, y_true, y_pred_probs, total_images=9):
     """
-    Display random test images along with predictions and true labels based on prior predictions and TensorFlow dataset.
+    Wyświetla losowe obrazy testowe wraz z przewidywaniami i etykietami prawdziwymi
+    na podstawie wcześniej wykonanych predykcji i datasetu TensorFlow.
 
     Args:
-    test_dataset: TensorFlow dataset containing test images.
-    y_true: True labels of the images.
-    y_pred_probs: Predicted probabilities for each class by the model.
-    total_images: Total number of images to display.
+    test_dataset: Dataset TensorFlow zawierający obrazy testowe.
+    y_true: Prawdziwe etykiety obrazów.
+    y_pred_probs: Prawdopodobieństwa przewidziane przez model dla każdej klasy.
+    total_images: Całkowita liczba obrazów do wyświetlenia.
     """
     y_pred = np.argmax(y_pred_probs, axis=1)
     random_indices = np.random.choice(range(len(y_true)), size=total_images, replace=False)
@@ -71,15 +73,17 @@ def display_random_images_with_predictions(test_dataset, y_true, y_pred_probs, t
     plt.tight_layout()
     plt.show()
 
+
 def display_misclassified_images(test_dataset, y_true, y_pred, total_images=9):
     """
-    Display a specified number of misclassified images based on prior predictions and TensorFlow dataset.
+    Wyświetla określoną liczbę błędnie sklasyfikowanych obrazów na podstawie wcześniej wykonanych predykcji
+    i datasetu TensorFlow.
 
     Args:
-    test_dataset: TensorFlow dataset containing test images.
-    y_true: True labels of the images.
-    y_pred: Predicted labels by the model.
-    total_images: Total number of misclassified images to display.
+    test_dataset: Dataset TensorFlow zawierający obrazy testowe.
+    y_true: Prawdziwe etykiety obrazów.
+    y_pred: Etykiety przewidziane przez model.
+    total_images: Całkowita liczba błędnie sklasyfikowanych obrazów do wyświetlenia.
     """
     misclassified_indices = np.where(y_pred != y_true)[0]
     selected_indices = np.random.choice(misclassified_indices, size=total_images, replace=False)
@@ -98,32 +102,33 @@ def display_misclassified_images(test_dataset, y_true, y_pred, total_images=9):
     plt.tight_layout()
     plt.show()
 
+
 def calculate_and_plot_roc_curve(y_true, y_pred_probs, n_classes):
     """
-    Micro ROC calculation.
+    Oblicza i rysuje krzywą ROC dla makro- i mikrośredniej.
     """
-    # Binarization of labels
+    # Binarizacja etykiet
     y_true_binarized = label_binarize(y_true, classes=range(n_classes))
     
-    # Calculate micro-average ROC
+    # Obliczanie mikrośredniej ROC
     fpr_micro, tpr_micro, _ = roc_curve(y_true_binarized.ravel(), y_pred_probs.ravel())
     roc_auc_micro = auc(fpr_micro, tpr_micro)
 
-    # Calculate macro-average ROC
+    # Obliczanie makrośredniej ROC
     roc_auc_macro = roc_auc_score(y_true_binarized, y_pred_probs, average='macro')
     fpr_macro, tpr_macro, _ = roc_curve(y_true_binarized.ravel(), y_pred_probs.ravel(), pos_label=1)
     
-    # Plot ROC curve for micro-average
+    # Rysowanie krzywej ROC dla mikrośredniej
     plt.figure(figsize=(8, 6))
     plt.plot(fpr_micro, tpr_micro, label='Micro-average ROC curve (area = {0:0.2f})'.format(roc_auc_micro))
 
-    # Plot ROC curve for macro-average
+    # Rysowanie krzywej ROC dla makrośredniej
     plt.plot(fpr_macro, tpr_macro, label='Macro-average ROC curve (area = {0:0.2f})'.format(roc_auc_macro), linestyle='--')
 
-    # Reference line
+    # Linia odniesienia
     plt.plot([0, 1], [0, 1], 'k--')
 
-    # Plot settings
+    # Ustawienia wykresu
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
@@ -132,9 +137,10 @@ def calculate_and_plot_roc_curve(y_true, y_pred_probs, n_classes):
     plt.legend(loc="lower right")
     plt.show()
 
+
 def calculate_and_plot_precision_recall_curve(y_true, y_pred_probs, n_classes):
     """
-    Calculate and draw precision-recall curve.
+    Oblicza i rysuje krzywą Precision-Recall.
     """
     y_true_binarized = label_binarize(y_true, classes=range(n_classes))
     precision = dict()
@@ -158,12 +164,13 @@ def calculate_and_plot_precision_recall_curve(y_true, y_pred_probs, n_classes):
     plt.legend(loc="lower left")
     plt.show()
 
+
 def plot_training_history_class(history):
     """
-    Visualize the accuracy and loss history during training and validation.
+    Wizualizuje historię dokładności i straty podczas treningu i walidacji.
 
     Args:
-    history: History object returned from the model's fit() method.
+    history: Zwrócony obiekt History z metody fit() modelu.
     """
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
